a371ff68e3b5618b1b9d24c913bf9873211ecd18
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-14 15:16:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ class Artist
   end
 
   def songs
-    Song.all.collect {|song| song.artist.name == self.name}
+    Song.all.collect {|song| song.artist == self}
   end
 
   def self.find_or_create_by_name(name)
